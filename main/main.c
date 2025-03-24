248d2b8c38eccf1f86f32d2519a8eff26d9b10d0
gretafg <gretafg@al.insper.edu.br>
2025-03-24 13:47:22 -0300

    lab 4

--- main/main.c
+++ main/main.c
@@ -20,6 +20,73 @@ const uint LED_1_OLED = 20;
 const uint LED_2_OLED = 21;
 const uint LED_3_OLED = 22;
 
+const uint PIN_TRIGGER = 2;
+const uint PIN_ECHO = 3;
+
+QueueHandle_t xQueueTime;
+QueueHandle_t xQueueDistance;
+SemaphoreHandle_t xSemaphoreTrigger;
+
+void pin_callback(uint gpio, uint32_t events) {
+    uint32_t time = to_us_since_boot(get_absolute_time());
+    if (events == 0x8) { 
+        uint32_t start_time = time;
+    } else {
+        uint32_t diff = time - start_time;
+        xQueueSendFromISR(xQueueTime, &diff, 0);
+        xSemaphoreGiveFromISR(xSemaphoreTrigger, 0);  
+    }
+}
+
+void trigger_task(void *p){
+    gpio_init(PIN_TRIGGER);
+    gpio_set_dir(PIN_TRIGGER, GPIO_OUT);
+
+    gpio_put(PIN_TRIGGER, 1);
+    sleep_us(10);
+    gpio_put(PIN_TRIGGER, 0);
+}
+
+void echo_task(void *p){
+    gpio_init(PIN_ECHO);
+    gpio_set_dir(PIN_ECHO, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(PIN_ECHO, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &echo_callback);
+
+    int duracao = 0;
+    while (true){
+        if (xQueueReceive(xQueueTime, &duracao, 0)){
+            float distancia = (duracao * 0.0343) /2;
+            xQueueSend(xQueueDistance, distancia, 0);
+        }
+    }
+}
+
+void oled_task(void *p){
+    printf("Inicializando Driver\n");
+    ssd1306_init();
+
+    printf("Inicializando GLX\n");
+    ssd1306_t disp;
+    gfx_init(&disp, 128, 32);
+
+    printf("Inicializando btn and LEDs\n");
+    oled1_btn_led_init();
+
+    char cnt = 15;
+    float distancia = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphoreTrigger, 0)) {
+            if (xQueueReceive(xQueueDistance, &distancia, 0)) {
+                gfx_clear_buffer(&disp);
+                char dist_str[20];
+                snprintf(dist_str, sizeof(dist_str), "Dist: %2.f cm", distancia);
+                gfx_draw_string(&disp, 0, 0, 1, dist_str);
+                gfx_show(&disp);
+            }
+        }
+    }
+}
+
 void oled1_btn_led_init(void) {
     gpio_init(LED_1_OLED);
     gpio_set_dir(LED_1_OLED, GPIO_OUT);
